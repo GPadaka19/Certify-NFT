541fbd3a88666c7172e0ec8d2682ce18ba3cf402
GPadaka19 <gustipadaka19@gmail.com>
2025-06-08 10:22:17 +0700

    feat: enhance CertificateNFT contract with certificate issuance event and updated minting function

--- contracts/CertificateNFT.sol
+++ contracts/CertificateNFT.sol
@@ -4,29 +4,46 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract CertificateNFT is ERC721URIStorage, AccessControl {
+contract CertificateNFT is ERC721, ERC721URIStorage, AccessControl {
     bytes32 public constant ISSUER_ROLE = keccak256("ISSUER_ROLE");
 
     uint256 private _tokenIdCounter;
+
+    event CertificateIssued(address indexed recipient, uint256 indexed tokenId, string certificateType);
 
     constructor() ERC721("CertificateNFT", "CERT") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(ISSUER_ROLE, msg.sender);
     }
 
-    function mint(address to, string memory tokenURI) public onlyRole(ISSUER_ROLE) {
+    function mint(address to, string memory uri, string memory certificateType) public onlyRole(ISSUER_ROLE) {
         uint256 tokenId = _tokenIdCounter;
         _safeMint(to, tokenId);
-        _setTokenURI(tokenId, tokenURI);
+        _setTokenURI(tokenId, uri);
         _tokenIdCounter++;
+        
+        emit CertificateIssued(to, tokenId, certificateType);
     }
 
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(AccessControl, ERC721URIStorage)
+        override(AccessControl, ERC721, ERC721URIStorage)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
     }
 }
